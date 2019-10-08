932b0cadc0e8a8c212c55746876f64c845cda668
Enigma7761 <47766594+Enigma7761@users.noreply.github.com>
2019-10-07 20:40:40 -0700

    Update FoundationMover.java

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FoundationMover.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FoundationMover.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.robotcontroller.internal;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 /*
@@ -12,8 +13,16 @@ public class FoundationMover extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
+        double slowspeed = .7;
+        boolean slowDrive = false;
+        float rightDrive, leftDrive;
+
         //Declaring the servo
         Servo servo = hardwareMap.get(Servo.class, "Servo");
+        DcMotor rightFrontMotor = hardwareMap.get(DcMotor.class, "rightFrontMotor");
+        DcMotor leftFrontMotor = hardwareMap.get(DcMotor.class, "leftFrontMotor");
+        DcMotor rightBackMotor = hardwareMap.get(DcMotor.class, "rightBackMotor");
+        DcMotor leftBackMotor = hardwareMap.get(DcMotor.class, "leftBackMotor");
 
         waitForStart();
         telemetry.addData("Status:", "Starting FoundationMover");
@@ -27,9 +36,33 @@ public class FoundationMover extends LinearOpMode {
 
             if(gamepad1.b == true){
                 //Resets the servo
-                servo.setPosition(1);
+                servo.setPosition(0);
                 telemetry.addData("Resetting Servo:", "0 degrees");
             }
+            rightDrive = gamepad1.right_stick_y;
+            leftDrive = gamepad1.left_stick_y;
+
+            if(gamepad1.dpad_up == true){
+                slowDrive = false;
+            }
+
+            if (gamepad1.dpad_down == true){
+                slowDrive = true;
+            }
+
+            if (slowDrive == true){
+                rightDrive *= slowspeed;
+                leftDrive *= slowspeed;
+            }
+
+            rightFrontMotor.setPower(-Math.abs(rightDrive)*rightDrive);
+            rightBackMotor.setPower(Math.abs(rightDrive)*rightDrive);
+            leftFrontMotor.setPower(-Math.abs(leftDrive)*leftDrive);
+            leftBackMotor.setPower(Math.abs(leftDrive)*leftDrive);
+
+            telemetry.addData("right power: ", rightDrive);
+            telemetry.addData("left power: ", leftDrive);
+            telemetry.addData("drive mode: ", slowDrive);
             telemetry.update();
         }
 
