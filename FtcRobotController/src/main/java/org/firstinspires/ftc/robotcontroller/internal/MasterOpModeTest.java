4b0dc4d7f25db55b4ca117a06d00d36f0ff6b705
Enigma7761 <47766594+Enigma7761@users.noreply.github.com>
2019-11-25 20:14:09 -0800

    Updates

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/MasterOpModeTest.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/MasterOpModeTest.java
@@ -1,5 +1,4 @@
 package org.firstinspires.ftc.robotcontroller.internal;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp(name = "MasterOpModeTest" , group = "Main Op Modes")
@@ -17,6 +16,7 @@ public class MasterOpModeTest extends Robot{
         float rightDriveY, leftDriveY, rightDriveX, leftDriveX;
         double angle = 0;
         int topPos = 6000;
+        boolean mecDrive = false;
 
 
         waitForStart();
@@ -25,22 +25,21 @@ public class MasterOpModeTest extends Robot{
 
             rightDriveY = gamepad1.right_stick_y;
             leftDriveY = gamepad1.left_stick_y;
-            leftDriveX = gamepad1.left_stick_x;
-            rightDriveX = gamepad1.left_stick_x;
+            rightDriveX = gamepad1.right_stick_x;
 
             //right bumper closes the claw and vice versa
-            if (gamepad1.right_bumper) {
-                TurnClaw(angle);
+            if (gamepad2.right_bumper) {
+                TurnClaw(0.55);
             }
-            if(gamepad1.left_bumper){
+            if(gamepad2.left_bumper){
                 ResetClaw();
             }
 
             //y lifts and a lowers lift, otherwise stop the lift
-            if(gamepad1.y && getPosLift() >= 0) {
+            if(gamepad2.a && getPosLift() >= 0) {
                 Lift(1);
             }
-            else if(gamepad1.a && getPosLift()  <= topPos) {
+            else if(gamepad2.y && getPosLift()  <= topPos) {
                 Lift(-1);
             }
             else {
@@ -49,10 +48,10 @@ public class MasterOpModeTest extends Robot{
 
 
             //b closes the foundation servo and x opens it.
-            if (gamepad1.b == true) {
-                TurnFoundationServo(angle);
+            if (gamepad2.b == true) {
+                TurnFoundationServo(0.5);
             }
-            if(gamepad1.x){
+            if(gamepad2.x){
                 ResetFoundationServo();
             }
 
@@ -68,27 +67,35 @@ public class MasterOpModeTest extends Robot{
             if (slowDrive == true) {
                 rightDriveY *= SLOWSPEED;
                 leftDriveY *= SLOWSPEED;
-                rightDriveX *= SLOWSPEED;
-                leftDriveX *= SLOWSPEED;
+//                rightDriveX *= SLOWSPEED;
             }
 
-            if(gamepad1.dpad_right){
-                angle += 0.01;
+/*
+            if(gamepad1.right_stick_button){
+                mecDrive = true;
             }
+            if(gamepad1.left_stick_button){
+                mecDrive = false;
+            }
+
+ */
+
             if(gamepad1.dpad_left){
-                angle -= 0.01;
+                MecanumDrive(-1);
             }
-
-            if(){
+            else if(gamepad1.dpad_right){
+                MecanumDrive(1);
+            }
+            else{
                 TankDrive(rightDriveY, leftDriveY);
             }
 
 
-
-
             telemetry.addData("Lift Pos:", getPosLift());
             telemetry.addData("Wheel Pos:", getPosWheel());
-            telemetry.addData("Angle",angle);
+            telemetry.addData("Angle: ",angle);
+            telemetry.addData("Slow Drive: ", slowDrive);
+            telemetry.addData("Mecanum Drive", mecDrive);
             telemetry.update();
         }
     }
--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Robot.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Robot.java
@@ -4,10 +4,43 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+import android.graphics.Bitmap;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.RobotLog;
+import com.qualcomm.robotcore.util.ThreadPool;
+import com.vuforia.Frame;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.function.Consumer;
+import org.firstinspires.ftc.robotcore.external.function.Continuation;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.matrices.MatrixF;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 public abstract class Robot extends LinearOpMode{
     private Servo foundationServo, clawServo;
     private DcMotor rightFrontMotor,leftFrontMotor, rightBackMotor, leftBackMotor, liftMotorLeft, liftMotorRight;
     private int posLift, posWheel;
+    //private WebcamName webcam;
 
     public void InitializeHardware(){
         //Hardware initiationlization
@@ -22,6 +55,8 @@ public abstract class Robot extends LinearOpMode{
 
         liftMotorLeft = hardwareMap.get(DcMotor.class, "liftMotorLeft");
         liftMotorRight = hardwareMap.get(DcMotor.class, "liftMotorRight");
+
+        //webcam  = hardwareMap.get(WebcamName.class,"Camera");
 
         // Lift initialize
         liftMotorRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -47,6 +82,9 @@ public abstract class Robot extends LinearOpMode{
         // Getting current positions
         posLift = liftMotorLeft.getCurrentPosition();
         posWheel = leftBackMotor.getCurrentPosition();  // ???
+        //int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        //VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+        //parameters.vuforiaLicenseKey = "AT0vdpL/////AAABmYfRfMJ4SEYKmqchn8mtyN4cj9kPD9y8s0MERpQOBt8B8ALWw/IuTuMrIMGp4bayqI7a4aAiW8Wx+Orm9xZ5O8K7mdmXrtFUuN0GF/agS/M6Sz4s4bJrgGio3Aauy1Shd18cGy7NWW4thm+vDQlOGgIAR0FEOEt4nv2RtcaK24Cusr4VgwGldY9/v6O7XGOwa4uAcR6Q3oI+wkuighO4Ag1wFAd3GyAmFMXTQvw41xbeNSZMOHpBJLx7eU348xWsaUNcdUoE02xyH/xZ6qUjNcDLcNnvP/+0LwFaRolPmNqCFfmB2cdgYsJfvkc32xgTSYALJYRi90bG72G/e2U9cs2Na2VzopyJM6a6nljBiQq9";
     }
 
     private float ScalePower(float power){
@@ -62,12 +100,12 @@ public abstract class Robot extends LinearOpMode{
         leftBackMotor.setPower(ScalePower(power2));
     }
 
-    public void MecanumDrive(float power1, float power2){
+    public void MecanumDrive(float power1){
         //Sets the power for the right to power1 scaled and left power2 scaled
-        rightFrontMotor.setPower(ScalePower(power1));
-        rightBackMotor.setPower(-ScalePower(power1));
-        leftFrontMotor.setPower(-ScalePower(power2));
-        leftBackMotor.setPower(ScalePower(power2));
+        rightFrontMotor.setPower(-ScalePower(power1));
+        rightBackMotor.setPower(ScalePower(power1));
+        leftFrontMotor.setPower(ScalePower(power1));
+        leftBackMotor.setPower(-ScalePower(power1));
     }
 
     public void TurnClaw(){
